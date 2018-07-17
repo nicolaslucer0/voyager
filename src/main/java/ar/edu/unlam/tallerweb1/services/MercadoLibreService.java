d05f8b012824a6eb326046063970481a4e3702ae
Martin Currao <mcurrao@gmail.com>
2018-07-16 23:45:42 -0300

    Se agrega documentacion de comportamiento al servicio de MercadoLibre.

--- src/main/java/ar/edu/unlam/tallerweb1/services/MercadoLibreService.java
+++ src/main/java/ar/edu/unlam/tallerweb1/services/MercadoLibreService.java
@@ -4,5 +4,14 @@ import ar.edu.unlam.tallerweb1.dto.mercadolibre.MLItem;
 
 public interface MercadoLibreService {
 
+	/**
+	 * Obtiene un {@link MLItem} de MercadoLibre en base a su id.
+	 * Arrojara una excepcion en caso de que el item no haya podido
+	 * ser hallado.
+	 * 
+	 * @param itemId ID de MercadoLibre del item a buscar. Tienen un formato
+	 * similar a 'MLA684928268'.
+	 * @return
+	 */
     public MLItem getItemDataByItemId(String itemId);
 }
